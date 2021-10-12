d0486d258f7b627e8f78843ae30ce712a2b5834d
Alexey Kapitonov <a.kapitonov@propellerads.net>
2021-10-12 20:34:41 +0300

    Solve 04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,16 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const app = createApp({
+  data() {
+    return {
+      selectedTab: null,
+      meetupData: '',
+    };
+  },
+  watch: {
+    async selectedTab(tab) {
+      this.meetupData = await fetchMeetupById(tab);
+    },
+  },
+}).mount('#app');
