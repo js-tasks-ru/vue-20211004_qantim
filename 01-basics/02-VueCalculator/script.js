82930d90c91ab950ece557051c14e6810b98f34a
Alexey Kapitonov <a.kapitonov@propellerads.net>
2021-10-12 19:51:05 +0300

    Solve 02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      first: 0,
+      second: 0,
+      operator: 'sum',
+      result: 0,
+    };
+  },
+  computed: {
+    calc() {
+      switch (this.operator) {
+        case 'sum':
+          return this.first + this.second;
+        case 'subtract':
+          return this.first - this.second;
+        case 'multiply':
+          return this.first * this.second;
+        case 'divide':
+          return this.first / this.second;
+        default:
+          return null;
+      }
+    },
+  },
+}).mount('#app');
